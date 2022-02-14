f09b35ad8a79dc2f96df2c8b124dfc63e088bf2f
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-02-14 17:56:50 -0300

    Instalado react router atualizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-//import { Home } from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { BrowserRouter, Route } from 'react-router-dom';
-
+import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <NewRoom />
-    </BrowserRouter>
-   
+  <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms/new" element={<NewRoom />} />
+      </Routes>
+    </BrowserRouter>    
   );
 }
 
