b35aacf1588a3250c58d5719b7b9ff748ef9d94c
FernandoLelis <fernandolelisdarocha@gmail.com>
2022-11-27 12:01:41 -0300

    Atulizacao - 27/11/2022

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 // Importando imagens (webpack)
+import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import illustrationImg from '../assets/images/illustration.svg';
@@ -7,12 +8,15 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 import '../styles/auth.scss';
+
 
 export function Home() {
     const navigate = useNavigate();
     const { user, signInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
     
 
     async function handleCreateRoon() {
@@ -21,6 +25,23 @@ export function Home() {
         }
         navigate('/rooms/new'); 
 
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exists.');
+            return;
+        }
+
+        navigate(`/rooms/${roomCode}`)
     }
 
     return(
@@ -38,10 +59,12 @@ export function Home() {
                         Crie sua sala com o google
                     </button>
                     <div className="separator">ou entre em uma sala</div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input 
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
